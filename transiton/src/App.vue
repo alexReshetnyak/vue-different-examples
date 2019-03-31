1fe6d183d532a5473b79cda266bfbb4bff425432
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-03-31 18:27:53 +0300

    worked on transitions animation

--- transiton/src/App.vue
+++ transiton/src/App.vue
@@ -8,6 +8,10 @@
 				<div class="p-3 mb-2 bg-success text-white" v-if="display">Hello</div>
 			</transition>
 
+			<transition name="slideup">
+				<div class="p-3 mb-2 bg-success text-white" v-if="display">Hello 2</div>
+			</transition>
+
 			<button class="btn btn-primary" @click="display = !display">Toggle animation</button>
 		</div>
 		<app-footer/>
@@ -38,11 +42,12 @@
 	}
 
 	.appear-enter {
+		transform: translateX(300px);
 		opacity: 0;
 	}
 
 	.appear-enter-active {
-		transition: opacity 1s;
+		transition: all 0.5s ease;
 	}
 
 	.appear-leave {
@@ -50,7 +55,45 @@
 	}
 
 	.appear-leave-active {
-		
+		transform: translateX(300px);
+		opacity: 0;
+		transition: all 0.5s ease;		
+	}
+
+
+	.slideup-enter {
+		opacity: 0;
+	}
+
+	.slideup-enter-active {
+		transition: opacity 0.5s;
+		animation: slideUp 0.5s ease-out;
+	}
+
+	.slideup-leave {}
+
+	.slideup-leave-active {
+		opacity: 0;
+		transition: opacity 0.5s;
+		animation: slideDown 0.5s ease-out;
+	}
+
+	@keyframes slideUp {
+		from {
+			transform: translateY(200px);
+		}
+		to {
+			transform: translateY(0);
+		}
+	}
+
+	@keyframes slideDown {
+		from {
+			transform: translateY(0);
+		}
+		to {
+			transform: translateY(200px);
+		}
 	}
 
 </style>
