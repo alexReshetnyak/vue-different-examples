6e5cce6d47560a6a7f6080e921721b149a926a82
A Reshetnyak <alexreshetnyakmail@gmail.com>
2019-04-12 16:47:23 +0300

    added vue-resource to pizza

--- pizza-app/src/components/menu/Menu.vue
+++ pizza-app/src/components/menu/Menu.vue
@@ -5,13 +5,61 @@
     </div>
 
     <div class="buttons">
-      
+      <mdb-btn 
+        size="sm" 
+        color="white"
+        @click="changeMenu(button.name)"
+        :active="button.active"
+        v-for="(button, index) in buttons" :key="index"
+      >
+        {{button.name}}
+      </mdb-btn>
     </div>
   </div>
 </template>
 
 <script>
+ import { mdbBtn } from 'mdbvue';
+
   export default {
-    
+    components: {
+      mdbBtn
+    },
+    data() {
+      return {
+        buttons: [
+          { name: 'pizza', active: true },
+          { name: 'burgers', active: false },
+          { name: 'salat', active: false },
+          { name: 'dessert', active: false }
+        ],
+        products: []
+      }
+    },
+    methods: {
+      changeMenu(value) {
+        this.buttons.forEach(button => {
+          button.active = button.name === value ? true : false; 
+        });
+      }
+    },
+    async created() {
+      try {
+        const { body: productList } = await this.$http.get('products.json');
+
+        this.products = Object.keys(productList).map(productId => ({ 
+          id: productId, 
+          ...productList[productId] 
+        }));
+      } catch (error) {
+        console.error(error);
+      }
+    },
   }
 </script>
+
+<style scoped>
+  .menu_container {
+    flex-direction: column;
+  }
+</style>
--- pizza-app/src/main.js
+++ pizza-app/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueResource from 'vue-resource';
 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbvue/build/css/mdb.css';
+
+Vue.use(VueResource);
+
+Vue.http.options.root = "https://vue-pizza-d9b05.firebaseio.com";
 
 new Vue({
   el: '#app',
