8228e537e2a0c7b32699c3cf77fbcbcb7c4e8c7c
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-04-14 17:26:13 +0300

    was created routes application

---
+++ routes/src/Components/Header_footer/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <header>
+            <div class="logo">Myweb.com</div>
+            <nav>
+                <span>Home</span>
+                <span>User</span>
+                <span>About us</span>
+            </nav>
+        </header>
+    </div>
+</template>
+
+
+<script>
+</script>
+
+<style scoped>
+
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+.logo {
+  float: left;
+}
+nav {
+  float: right;
+}
+nav span {
+  background: #3aa8ff;
+  font-size: 20px;
+  padding: 5px 10px;
+}
+
+header {
+  background: #2196f3;
+  border-bottom: 4px solid #607d8b;
+  box-sizing: border-box;
+  padding: 30px;
+  color: #ffffff;
+  font-size: 30px;
+}
+
+header:after {
+  content: ".";
+  visibility: hidden;
+  display: block;
+  height: 0;
+  clear: both;
+}
+</style>
---
+++ routes/src/Components/User.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>User</h1>
+        <hr>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Francis Jones</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip </p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
