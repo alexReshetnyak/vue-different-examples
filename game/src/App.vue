f56423183497447bafe95cafb12675d068f2aa5b
Alexey Reshetnyak <alexreshetnyakmail@gmail.com>
2019-04-22 18:15:07 +0300

    was created header and footer components

--- game/src/App.vue
+++ game/src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <comp-header />
+    <div>
+      content
+    </div> 
+    <comp-footer />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import compHeader from './components/Header-Footer/Header';
+import compFooter from './components/Header-Footer/Footer';
+
+export default {
+  components: {
+    compHeader,
+    compFooter
   }
 }
+</script>
+
+<style lang="scss">
+  @import './assets/style.scss';
 </style>
---
+++ game/src/components/Header-Footer/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <div class="container header_container">
+      <div class="logo">
+        Logo
+      </div>
+
+      <div class="login">
+        Login
+      </div>
+    </div>
+  </header>
+</template>
